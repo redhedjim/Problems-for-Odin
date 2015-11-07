1905e6de80477142ddf12bc3d1ab31a4ed25838c
Leo <jamesleotucker@gmail.com>
2015-11-06 21:31:22 -0700

    Update functions.js
    It was missing some. Now it's all better.

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-function fibonacci(){
+function fibonacci(number){
     // takes the fibonacci sequence numbers 1-4000000 and adds together the even numbers
     var a = 0;
     var b = 1;
@@ -13,7 +13,7 @@ function fibonacci(){
         a = b;
         b = c;
         total = c;
-        if(total%2 === 0 && total < 4000000){
+        if(total%2 === 0 && total < number){
         doc.innerHTML = doc.innerHTML + total+",";
             added = total+total;
         }
@@ -21,7 +21,8 @@ function fibonacci(){
     }
 };
 
-function largestPrimeFactor(number){
+function allPrimesBetween(number){
+    //finds all prime numbers between 2 and number parameter
     for (x = number; x > 2; x--){ 
       count = 0;    
         for(y = x-1; y > 1; y--){        
@@ -37,4 +38,38 @@ function largestPrimeFactor(number){
     placee.innerHTML = primeArray;     
 }
 
-largestPrimeFactor(20);
+allPrimesBetween(20);
+
+
+function largestPrimeFactor(number){
+//find all prime numbers under number parameter and what the largest prime factor fo that number is.
+    var place = document.getElementById('a');
+    var placee = document.getElementById('e');
+    var answer = 0;
+    var count = 0;
+    var prime = 0;
+    var highPrime = 0;
+    var primeArray = new Array();
+
+        for (x = number; x > 2; x--){ 
+          count = 0;    
+            for(y = x-1; y > 1; y--){        
+                if(x%y === 0){
+                    answer = x;
+                    count++; 
+                }    
+            }
+            if(count === 0){
+                primeArray.push(x);
+            }
+        }
+        placee.innerHTML = primeArray;
+        for(i = 0; i < primeArray.length; i++){
+            if(number % primeArray[i] === 0 && primeArray[i] > highPrime){
+                highPrime = primeArray[i];
+            }
+        }
+       place.innerHTML = highPrime;
+       console.log(highPrime);
+}
+largestPrimeFactor(28);
